73ab8f76e85a92b2b30d16ac72608be9c6eb0ac2
unknown <gauravpal95106@gmail.com>
2022-12-04 02:02:59 +0530

    adding sass and using map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import categories from "./categories.json"
+import './categories.styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <div className="categories-container">
+        {categories.map(({title, id, imageUrl}) => (
+          <div key={id} className="category-container">
+            <div className="background-image"style={{
+              backgroundImage: `url(${imageUrl})`
+            }} />
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
